8fcd1ca0a6ad6ea6eb6a7c43526e4b8e38ab5033
Mumu Wu <vwumumu@gmail.com>
2023-02-21 22:10:27 +0800

    add send button to group

--- index.js
+++ index.js
@@ -4,19 +4,35 @@ const { appendFileSync } = require("fs");
 const { BlazeClient } = require("mixin-node-sdk");
 const client = new BlazeClient(config, { parse: true, syncAck: true });
 
-const group = "b8fa64a3-1725-428b-9be7-30e1f7f9aaf8";
+const group = "9fe64c07-71d1-44a2-85c1-3694e1ede7ee";
 const receivers = [
-  "cbb20923-9020-490a-b8f6-e816883c9c99",
-  "d86ca453-e506-407a-ae61-e73bad0e4331",
+  //小熊
+  "f264c736-7a90-4475-a253-981d654c1e21",
+  //爱梅
+  "a6c8155c-9493-4034-84a8-83f8edc56cf6",
+  //阿坦
+  "fedd7458-bdfc-488c-87be-6225ee3cf523",
+  //志全
+  "9dcd64cf-d753-4c53-87d0-bc011fd94cb3",
+  //放鹅娃
+  "9b483aab-eda5-49ff-b111-328206dedb1a",
+  //机器人
   client.keystore.client_id,
 ];
-const threshold = 2;
+const threshold = 3;
 const mumu = "cbb20923-9020-490a-b8f6-e816883c9c99";
 
 client.loopBlaze({
   async onMessage(msg) {
     if (msg.user_id === mumu) {
       client.sendMessageText(mumu, msg.conversation_id);
+    }
+    if (
+      msg.data.toString().substring(0, 11) === "@7000104916" &&
+      msg.data.toString().substring(11).trim() === "$"
+    ) {
+      // await transferButton();
+      console.log("$");
     }
   },
   async onTransfer(msg) {
@@ -66,3 +82,40 @@ async function toMultiSignAddress({ data, created_at }, user) {
   }`;
   await toGroup(msg_recive_crypto, group);
 }
+
+async function transferButton() {
+  const transferAction = `mixin://transfer/${client.keystore.client_id}`;
+  await client.sendAppButtonMsg(
+    // 给用户发送转账的 button
+    user_id,
+    [
+      {
+        label: `Transfer to ${client.keystore.client_id}`,
+        action: transferAction,
+        color: "#000",
+      },
+    ]
+  );
+}
+
+// let data = [{
+//   label: "签到",
+//   action: "input:@7000104895/claim",
+//   color: "#FF0000",
+// }];
+
+// data = JSON.stringify(data);
+
+// console.log(data);
+
+// client
+//   .sendMessage({
+//     conversation_id: group,
+//     message_id: client.newUUID(),
+//     category: "APP_BUTTON_GROUP",
+//     data: Buffer.from(data).toString("base64url"),
+//     // data: Base64.encode(data)
+//   })
+//   .then(console.log);
+
+
